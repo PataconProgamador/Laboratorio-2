08d4ad569275a67fd42f15e0d9b764476fcde00e
PataconProgamador <luiscarlos.jimenez@utp.ac.pa>
2026-08-30 21:30:08 -0500

    Add files via upload

---
+++ Problema 3/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problema_3
+{
+    public class LibroCalificaciones
+    {
+        private string nombreDelCurso;
+
+        public LibroCalificaciones(string nombre)
+        {
+            nombreDelCurso = nombre;
+        }
+        
+        public string NombreCurso 
+        {
+            get { return nombreDelCurso;  }
+            set { nombreDelCurso = value; }
+        }
+        public void MostrarMensaje()
+        {
+            Console.WriteLine($"Bienvenido al libro de calificaciones para \n{0}!", nombreDelCurso);
+        }
+    }
+}
---
+++ Problema 3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Problema_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            LibroCalificaciones mylibro = new LibroCalificaciones("CS101 Programación en C#");
+            LibroCalificaciones mylibro2 = new LibroCalificaciones("CS102 Estructuras de Datos");
+            Console.WriteLine("El Nombre del Curso es: {0}", mylibro.NombreCurso);
+            Console.WriteLine("El Nombre del Curso es: {0}", mylibro2.NombreCurso);
+
+
+            //Pide y lee el nombre del curso
+            Console.WriteLine("\n Escriba el nombre del curso: ");
+            string elNombrecurso = Console.ReadLine();
+            mylibro.NombreCurso = elNombrecurso;
+            Console.WriteLine("\n El nombre del curso es: {0}", mylibro.NombreCurso);
+        }
+    }
+}
